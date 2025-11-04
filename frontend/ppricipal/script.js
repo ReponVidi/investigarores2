750ab7a51decbc5e79a13c72e48fe5f8e4d97f83
ReponVidi <marcpasante2025@gmail.com>
2025-11-04 11:43:32 -0400

    Cambios realizados al frotend;
    1. Modificacion del Boton de Inicio de Sesion; se comento porque aun no lo estamos implementando
    
    2. Configuracion del boton de Registrate para que redireccione al usuario al portal de la creacion de usuario

--- frontend/ppricipal/script.js
+++ frontend/ppricipal/script.js
@@ -10,3 +10,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     setInterval(nextSlide, 5000);
 });
+
+// Esperar que el documento cargue completamente
+document.addEventListener("DOMContentLoaded", () => {
+    const botonRegistro = document.querySelector(".registrate");
+    
+    if (botonRegistro) {
+        botonRegistro.addEventListener("click", () => {
+            // Redirige a la página de inicio de sesión
+            window.location.href = "../inicio_sesion/inicio.html";
+        });
+    }
+});
