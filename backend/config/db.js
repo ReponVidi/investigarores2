13e600f9969f10ef842a4f3cddbc7b8818a76255
ReponVidi <marcpasante2025@gmail.com>
2026-04-22 12:14:44 -0400

    ARCHIVO PROJECT ROUTS ELIMINADA

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,20 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-/*const pool = new Pool({
-    host: "10.0.13.229",
+const pool = new Pool({
+    host: "localhost",
     user: "openprouser",
     password: "M10A07Q12U",
     database: "openprojectdb",
     port: 5432
-});*/
+});
 
-const pool = new Pool({
+/*const pool = new Pool({
     host: "localhost",
     user: "postgres",
     password: "sayed2819",
     database: "openprojectdb",
     port: 5432
-});
+});*/
 
 export default pool;
