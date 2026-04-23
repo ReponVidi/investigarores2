c09fa3458683d5259bbb87598ef0f9721c12ea56
ReponVidi <marcpasante2025@gmail.com>
2026-04-22 22:31:21 -0400

    actualizacion de creacion de proyecto 2

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,12 +9,21 @@ const { Pool } = pkg;
     port: 5432
 });*/
 
+
 const pool = new Pool({
+    host: "localhost",
+    user: "postgres",
+    password: "123456",
+    database: "open_project",
+    port: 5432
+});
+
+/*const pool = new Pool({
     host: "localhost",
     user: "postgres",
     password: "sayed2819",
     database: "openprojectdb",
     port: 5432
-});
+});*/
 
 export default pool;
